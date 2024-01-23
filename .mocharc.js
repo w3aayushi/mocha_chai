0f9b3e3721e25768a68973ff88ab3473577497e1
w3aayushi <aayushi.m@w3dev.email>
2024-01-23 12:43:07 +0000

    async functions

--- .mocharc.js
+++ .mocharc.js
@@ -1,3 +1,9 @@
 module.exports = {
-    // ui:'tdd'
+    ui:'tdd',
+    timeout: 2000,
+    slow: 150,
+    reporter: 'spec',
+    retries: 0,
+    ui: 'bdd',
+    'watch-files': ['test/**/*.js']
 }
--- test/async.spec.js
+++ test/async.spec.js
@@ -1,21 +1,29 @@
 const expect = require('chai').expect
 const axios = require('axios')
 
-describe('async test suite', ()=>{
-//     it('promised based way',()=>{
-//         return axios.get('https://reqres.in/api/users/2').then(res =>{
-//             expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in')
-//         })
-//     });
+describe('async test suite', function (){
 
-    // it('done based way',()=>{
-    //     axios.get('https://reqres.in/api/users/2').then(res =>{
-    //         expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in')
-    //         done()
-    //     }).catch(err =>{
-    //         done(err)
-    //     })
-    // });
+    this.retries(1)
+    let count = 0;
+
+    it('promised based way', function(){
+        this.timeout(100)
+        console.log('Retry attempt-', count);
+        count++ ;
+        this.retries(2)
+        return axios.get('https://reqres.in/api/users/2').then(res =>{
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in')
+        })
+    });
+
+    it('done based way', (done)=>{
+        axios.get('https://reqres.in/api/users/2').then(res =>{
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in')
+            done()
+        }).catch(err =>{
+            done(err)
+        })
+    });
 
     it('async await based way', async()=>{
         const res = await axios.get('https://reqres.in/api/users/2')
